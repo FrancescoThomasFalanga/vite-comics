2dbc14c3d210d19825711f11ff558d6544d596db
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-24 16:20:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    
+    
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
