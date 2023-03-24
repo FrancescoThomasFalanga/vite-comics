8ee1ceacb4d9af002d716774eb01a4fe8140bbd0
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-24 17:00:24 +0100

    AppHeader complete

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,12 +10,28 @@ export default {
 
 
 <template>
-    <div>
-        
-    </div>
+    <header>
+        <div class="container">
+            
+        <h2> --&gt Content goes here &lt--</h2>
+    
+        </div>
+    </header>
 </template>
 
 
 <style lang="scss" scoped>
+
+header {
+    background-color: #1c1c1c;
+
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 30px 0;
+        color: white;
+    }
+
+}
 
 </style>
