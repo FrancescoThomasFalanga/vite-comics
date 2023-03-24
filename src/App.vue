06a0df2ce2d157cb51a38418013bf4d82206d547
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-24 16:30:20 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
+import AppNavbar from "./components/AppNavbar.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
 export default {
   data() {
     return {
 
     }
+  },
+
+  components: {
+    AppNavbar,
+    AppHeader,
+    AppMain,
+    AppFooter,
   }
 }
 </script>
@@ -11,7 +23,15 @@ export default {
 
 <template>
   <div>
-    
+
+    <AppNavbar></AppNavbar>
+
+    <AppHeader></AppHeader>
+
+    <AppMain></AppMain>
+
+    <AppFooter></AppFooter>
+
     
   </div>
 </template>
