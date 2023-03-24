9493810a5c90b8d02cfb688884593fa8524ad5ce
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-24 16:54:00 +0100

    NavBar complete

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,6 @@ export default {
 
     <AppFooter></AppFooter>
 
-    
   </div>
 </template>
 
--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,7 +2,18 @@
 export default {
     data() {
         return {
-
+            links: [
+                "Character",
+                "Comics",
+                "Movies",
+                "Tv",
+                "Games",
+                "Collectibles",
+                "Videos",
+                "Fans",
+                "News",
+                "Shop",
+            ],
         }
     },
 }
@@ -10,12 +21,63 @@ export default {
 
 
 <template>
-    <div>
-        
-    </div>
+    <nav>
+        <div class="container">
+
+            <img src="/img/dc-logo.png" alt="">
+
+            <div>
+                <ul>
+                    <li v-for="link in links"> {{ link }} </li>
+                </ul>
+            </div>
+            
+        </div>
+    </nav>
 </template>
 
 
 <style lang="scss" scoped>
+
+nav {
+    background-color: white;
+
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0 auto;
+        max-width: 1200px;
+
+        img {
+            margin: 10px;
+            width: 60px;
+        }
+
+        ul {
+            display: flex;
+            list-style-type: none;
+
+            li {
+                cursor: pointer;
+                padding: 30px 10px;
+                font-size: 12px;
+                font-weight: bold;
+                text-transform: uppercase;
+
+                &:hover {
+                    color: #0282f9;
+                    border-bottom: 3px solid #0282f9;
+                }
+            }
+        }
+    }
+
+}
+
+#logo {
+    
+}
 
 </style>
