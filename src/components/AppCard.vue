209919c569479588c26ee8a8ee8f47233984e0c3
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-27 15:17:12 +0200

    complete

---
+++ src/components/AppCard.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        thumb: String,
+        title: String,
+    }
+}
+</script>
+
+
+<template>
+
+        <div class="card">
+            <img :src="thumb" alt="">
+            <span> {{ title }} </span>
+        </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+    .card {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 10px;
+        width: calc(100% / 6);
+        padding: 20px;
+
+        span {
+            text-transform: uppercase;
+            font-size: 14px;
+        }
+    }
+
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,88 @@
 <script>
+import AppCard from "./AppCard.vue"
+
 export default {
     data() {
         return {
-
+            dcCards: [
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://picsum.photos/100",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                }
+            ],
         }
+    },
+
+    components: {
+        AppCard,
     },
 }
 </script>
@@ -11,10 +90,17 @@ export default {
 
 <template>
     <header>
+
+        <div class="bg-img"></div>
+
         <div class="container">
-            
-        <h2> --&gt Content goes here &lt--</h2>
-    
+            <AppCard v-for="card in dcCards" class="ciao" :thumb="card.thumb" :title="card.series"></AppCard>
+
+            <div class="absolute">CURRENT SERIES</div>
+        </div>
+
+        <div class="more">
+            <a href="">LOAD MORE</a>
         </div>
     </header>
 </template>
@@ -25,11 +111,46 @@ export default {
 header {
     background-color: #1c1c1c;
 
+    .bg-img {
+        background-image: url("/img/jumbotron.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 400px;
+    }
+
     .container {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
         max-width: 1200px;
         margin: 0 auto;
         padding: 30px 0;
         color: white;
+
+        .absolute {
+            position: absolute;
+            top: -23px;
+            left: 0;
+            padding: 8px 30px;
+            font-weight: bold;
+            font-size: 20px;
+            background-color: #0282f9;
+        }
+    }
+
+    .more {
+        text-align: center;
+        padding-bottom: 20px;
+
+        a {
+            padding: 6px 60px;
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+            background-color: #0282f9;
+        }
     }
 
 }
